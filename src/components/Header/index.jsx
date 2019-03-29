e57e989a8b166885352bd9f7db5c48ee6def1267
Justin Brown <justinbrown.work@gmail.com>
2019-03-29 18:21:52 -0500

    Change header color.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Box, Heading } from "pcln-design-system";
 
 export const Header = () => {
   return (
-    <Box py={2} px={2} mb={2} bg="purple">
+    <Box py={2} px={2} mb={2} bg="darkBlue">
       <Heading color="white">Todo</Heading>
     </Box>
   );
