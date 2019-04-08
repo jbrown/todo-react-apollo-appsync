2f541f40fd3c6e6d70d6ae596670bfe3a3151f6e
Justin Brown <justinbrown.work@gmail.com>
2019-04-07 23:14:36 -0500

    Add link to Github repo in the header.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Heading } from "pcln-design-system";
-import { Box } from "../index";
+import { Flex } from "../index";
 
 export const Header = ({ ...props }) => {
   return (
-    <Box {...props} py={2} px={3} bg="darkBlue">
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      py={2}
+      px={3}
+      bg="darkBlue"
+      {...props}
+    >
       <Heading color="white">Todo</Heading>
-    </Box>
+      <a
+        href="https://github.com/jbrown/todo-react-apollo-appsync"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="View Github Repository"
+      >
+        <FontAwesomeIcon icon={faGithub} color="white" size="lg" />
+      </a>
+    </Flex>
   );
 };
